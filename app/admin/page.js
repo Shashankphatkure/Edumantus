1abd8e20032e5a49db039c59c3d1ef6c89e600d4
Shashank Phatkure <shashankphatkure@gmail.com>
2024-12-16 22:59:17 +0530

    updated admin

--- app/admin/page.js
+++ app/admin/page.js
@@ -76,11 +76,7 @@ export default function AdminDashboard() {
                   </button>
                 </div>
               </div>
-              <div className="flex items-center space-x-4">
-                <button className="bg-blue-600 text-white px-4 py-2 rounded-lg hover:bg-blue-700">
-                  New Expert
-                </button>
-              </div>
+              
             </div>
           </div>
         </nav>
